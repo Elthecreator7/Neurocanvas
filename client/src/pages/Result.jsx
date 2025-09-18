07e287cc57badb712c9a79ecfe9ec3824a7a3e7c
Elthecreator7 <enejecaleb@gmail.com>
2025-09-18 07:48:27 +0100

    More minor changes

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -74,7 +74,7 @@ const Result = () => {
             {
                 isImageLoaded &&
                 <div className='flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full'>
-                    <p className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer' onClick={() => { setIsImageLoaded(false) }}>Generate Another</p>
+                    <p className='bg-transparent border border-white text-white px-8 py-3 rounded-full cursor-pointer' onClick={() => { setIsImageLoaded(false) }}>Generate Another</p>
                     <a href={image} download className='bg-zinc-900 px-10 py-3 rounded-full cursor-pointer'>Download</a>
                 </div>
             }
